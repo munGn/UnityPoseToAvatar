41ff276b8e61d13b3f3423ac6065bd9e863399fb
ygr4789 <yongdori@snu.ac.kr>
2025-04-29 00:02:35 +0900

    feat: fix normalizedLandmark to worldLandmark

--- Assets/Scenes/LandmarkRig.cs
+++ Assets/Scenes/LandmarkRig.cs
@@ -7,7 +7,7 @@ public class LandmarkRig : MonoBehaviour
 {
     private Animator animator;
 
-    private enum Landmark
+    private enum LandmarkID
     {
         Nose = 0,
         LeftEyeInner = 1,
@@ -44,7 +44,7 @@ public class LandmarkRig : MonoBehaviour
         RightFootIndex = 32
     }
 
-    private Dictionary<Landmark, Vector3> landmarks;
+    private Landmarks landmarks;
     private Bone boneRightShoulder;
     private Bone boneRightUpperArm;
     private Bone boneRightLowerArm;
@@ -101,86 +101,84 @@ public class LandmarkRig : MonoBehaviour
 
     void Update()
     {
-        if (landmarks is null) return;
+        if (landmarks.landmarks is null) return;
         
-        boneRightUpperLeg.Update(landmarks[Landmark.RightHip], landmarks[Landmark.RightKnee]);
-        boneRightLowerLeg.Update(landmarks[Landmark.RightKnee], landmarks[Landmark.RightAnkle]);
-        boneRightFoot.Update(landmarks[Landmark.RightAnkle], landmarks[Landmark.RightHeel]);
-        boneLeftUpperLeg.Update(landmarks[Landmark.LeftHip], landmarks[Landmark.LeftKnee]);
-        boneLeftLowerLeg.Update(landmarks[Landmark.LeftKnee], landmarks[Landmark.LeftAnkle]);
-        boneLeftFoot.Update(landmarks[Landmark.LeftAnkle], landmarks[Landmark.LeftHeel]);   
-        boneRightUpperArm.Update(landmarks[Landmark.RightShoulder], landmarks[Landmark.RightElbow]);
-        boneRightLowerArm.Update(landmarks[Landmark.RightElbow], landmarks[Landmark.RightWrist]);
-        boneLeftUpperArm.Update(landmarks[Landmark.LeftShoulder], landmarks[Landmark.LeftElbow]);
-        boneLeftLowerArm.Update(landmarks[Landmark.LeftElbow], landmarks[Landmark.LeftWrist]);
+        boneRightUpperLeg.Update(LandmarkPos(LandmarkID.RightHip), LandmarkPos(LandmarkID.RightKnee), BonePresence(LandmarkID.RightHip, LandmarkID.RightKnee));
+        boneRightLowerLeg.Update(LandmarkPos(LandmarkID.RightKnee), LandmarkPos(LandmarkID.RightAnkle), BonePresence(LandmarkID.RightHip, LandmarkID.RightAnkle));
+        boneRightFoot.Update(LandmarkPos(LandmarkID.RightAnkle), LandmarkPos(LandmarkID.RightHeel), BonePresence(LandmarkID.RightHip, LandmarkID.RightHeel));
+        boneLeftUpperLeg.Update(LandmarkPos(LandmarkID.LeftHip), LandmarkPos(LandmarkID.LeftKnee), BonePresence(LandmarkID.RightHip, LandmarkID.LeftKnee));
+        boneLeftLowerLeg.Update(LandmarkPos(LandmarkID.LeftKnee), LandmarkPos(LandmarkID.LeftAnkle), BonePresence(LandmarkID.RightHip, LandmarkID.LeftAnkle));
+        boneLeftFoot.Update(LandmarkPos(LandmarkID.LeftAnkle), LandmarkPos(LandmarkID.LeftHeel), BonePresence(LandmarkID.RightHip, LandmarkID.LeftHeel));   
+        boneRightUpperArm.Update(LandmarkPos(LandmarkID.RightShoulder), LandmarkPos(LandmarkID.RightElbow), BonePresence(LandmarkID.RightHip, LandmarkID.RightElbow));
+        boneRightLowerArm.Update(LandmarkPos(LandmarkID.RightElbow), LandmarkPos(LandmarkID.RightWrist), BonePresence(LandmarkID.RightHip, LandmarkID.RightWrist));
+        boneLeftUpperArm.Update(LandmarkPos(LandmarkID.LeftShoulder), LandmarkPos(LandmarkID.LeftElbow), BonePresence(LandmarkID.RightHip, LandmarkID.LeftElbow));
+        boneLeftLowerArm.Update(LandmarkPos(LandmarkID.LeftElbow), LandmarkPos(LandmarkID.LeftWrist), BonePresence(LandmarkID.RightHip, LandmarkID.LeftWrist));
         
-        Vector3 neckPos = (landmarks[Landmark.RightShoulder] + landmarks[Landmark.LeftShoulder]) / 2;
-        Vector3 hipsPos = (landmarks[Landmark.RightHip] + landmarks[Landmark.LeftHip]) / 2;
+        Vector3 neckPos = (LandmarkPos(LandmarkID.RightShoulder) + LandmarkPos(LandmarkID.LeftShoulder)) / 2;
+        Vector3 hipsPos = (LandmarkPos(LandmarkID.RightHip) + LandmarkPos(LandmarkID.LeftHip)) / 2;
         
-        boneHead.Update(neckPos, landmarks[Landmark.Nose]);
+        boneHead.Update(neckPos, LandmarkPos(LandmarkID.Nose), true);
 
         Vector3 currentUpDown = (neckPos - hipsPos).normalized;
+        // Vector3 currentUpDown = Vector3.up;
         Quaternion deltaRotUpDown = Quaternion.FromToRotation(initialUpDown, currentUpDown);
 
-        Vector3 currentTwist = (landmarks[Landmark.RightHip] - landmarks[Landmark.LeftHip]).normalized;
+        Vector3 currentTwist = (LandmarkPos(LandmarkID.RightHip) - LandmarkPos(LandmarkID.LeftHip)).normalized;
         Quaternion deltaRotTwist = Quaternion.FromToRotation(initialTwist, currentTwist);
-        Vector3 currentShoulderTwist = (landmarks[Landmark.RightShoulder] - landmarks[Landmark.LeftShoulder]).normalized;
+        Vector3 currentShoulderTwist = (LandmarkPos(LandmarkID.RightShoulder) - LandmarkPos(LandmarkID.LeftShoulder)).normalized;
         Quaternion deltaRotShoulderTwist = Quaternion.FromToRotation(initialShoulderTwist, currentShoulderTwist);
         
         animator.GetBoneTransform(HumanBodyBones.Hips).rotation = deltaRotUpDown * deltaRotTwist * initialHipRotation;
         animator.GetBoneTransform(HumanBodyBones.Spine).rotation = deltaRotUpDown * Quaternion.Slerp(deltaRotTwist, deltaRotShoulderTwist, 0.5f) * initialSpineRotation;
         animator.GetBoneTransform(HumanBodyBones.Chest).rotation = deltaRotUpDown * deltaRotShoulderTwist * initialChestRotation;
-
     }
 
     public void SetPose(PoseLandmarkerResult result)
     {
-        landmarks ??= new Dictionary<Landmark, Vector3>();
-        var poseWorldLandmark = result.poseLandmarks[0];
-        
-        landmarks[Landmark.Nose] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.Nose]);
-        landmarks[Landmark.LeftShoulder] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.LeftShoulder]);
-        landmarks[Landmark.RightShoulder] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.RightShoulder]);
-        landmarks[Landmark.LeftElbow] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.LeftElbow]);
-        landmarks[Landmark.RightElbow] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.RightElbow]);
-        landmarks[Landmark.LeftWrist] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.LeftWrist]);
-        landmarks[Landmark.RightWrist] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.RightWrist]);
-        landmarks[Landmark.LeftHip] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.LeftHip]);
-        landmarks[Landmark.RightHip] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.RightHip]);
-        landmarks[Landmark.LeftKnee] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.LeftKnee]);
-        landmarks[Landmark.RightKnee] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.RightKnee]);
-        landmarks[Landmark.LeftAnkle] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.LeftAnkle]);
-        landmarks[Landmark.RightAnkle] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.RightAnkle]);
-        landmarks[Landmark.LeftHeel] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.LeftHeel]);
-        landmarks[Landmark.RightHeel] = LandmarkToVector(poseWorldLandmark.landmarks[(int)Landmark.RightHeel]);
+        var resultLandmarks  = result.poseWorldLandmarks[0];
+        resultLandmarks.CloneTo(ref landmarks);
     }
 
-    Vector3 LandmarkToVector(NormalizedLandmark landmark)
+    private Vector3 LandmarkPos(LandmarkID id)
     {
+        var landmark = landmarks.landmarks[(int)id];
         var x = landmark.x;
         var y = -landmark.y;
         var z = landmark.z;
         return new Vector3(x, y, z);
     }
+    
+    private bool BonePresence(LandmarkID id1, LandmarkID id2, float threshold = 0.2f)
+    {
+        var p1 = landmarks.landmarks[(int)id1].presence > threshold;
+        var p2 = landmarks.landmarks[(int)id2].presence > threshold;
+        return p1 && p2;
+    }
+
+    private class Bone
+    {
+        public Transform parent;
+        public Transform child;
+        public Vector3 initialDir;
+        public Quaternion initialRotation;
+        public Bone(Transform parent, Transform child) 
+        {
+            this.parent = parent;
+            this.child = child;
+            initialDir = (child.position - parent.position).normalized;
+            initialRotation = parent.rotation;
+        }
+        public void Update(Vector3 parentPos, Vector3 childPos, bool presence)
+        {
+            if (!presence)
+            {
+                parent.rotation = initialRotation;
+                return;
+            }
+            Vector3 currentDir = (childPos - parentPos).normalized;
+            Quaternion deltaRotTracked = Quaternion.FromToRotation(initialDir, currentDir);
+            parent.rotation = deltaRotTracked * initialRotation;
+        }
+    } 
 }
 
-class Bone 
-{
-    public Transform parent;
-    public Transform child;
-    public Vector3 initialDir;
-    public Quaternion initialRotation;
-    public Bone(Transform parent, Transform child) 
-    {
-        this.parent = parent;
-        this.child = child;
-        initialDir = (child.position - parent.position).normalized;
-        initialRotation = parent.rotation;
-    }
-    public void Update(Vector3 parentPos, Vector3 childPos)
-    {
-        Vector3 currentDir = (childPos - parentPos).normalized;
-        Quaternion deltaRotTracked = Quaternion.FromToRotation(initialDir, currentDir);
-        parent.rotation = deltaRotTracked * initialRotation;
-    }
-} 
